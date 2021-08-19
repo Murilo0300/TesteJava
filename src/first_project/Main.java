c136ae0c86d55649452db81ca14e12b754866d1b
Murilo Cavalcanti <murilo0300@hotmail.com.br>
2021-08-18 22:46:34 -0300

    first commit

---
+++ src/first_project/Main.java
@@ -0,0 +1,12 @@
+package first_project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("primeiro projeto");
+
+	}
+
+}
